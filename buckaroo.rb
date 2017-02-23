201868486b570c61b569dcf62c4473d909c01a4a
njlr <njlr@users.noreply.github.com>
2017-02-23 17:55:18 +0000

    * Removed .buckconfig generation since this is now in source-control

--- buckaroo.rb
+++ buckaroo.rb
@@ -6,16 +6,8 @@ class Buckaroo < Formula
   depends_on :java => "1.8+"
   depends_on 'buck'
   def install
-    buckconfig = File.new('.buckconfig', 'w')
-    buckconfig.puts("[download]\n" + 
-      "maven_repo = http://repo.maven.apache.org/maven2/\n" + 
-      "in_build = true\n")
-    buckconfig.close
     system 'buck', 'build', ':buckaroo-cli'
-    # File.new("build/successful-build", "w") {}
     libexec.install Dir["buck-out/gen/*"]
-    
-    # java -jar /usr/local/Cellar/buckaroo/HEAD-645da48/gen/buckaroo-cli.jar
     bin.write_jar_script libexec + "buckaroo-cli.jar", "buckaroo"
   end
 end
