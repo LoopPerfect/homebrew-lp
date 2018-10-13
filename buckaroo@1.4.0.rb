75c007d26b8027ef1ab19e1920d88f56846d346c
njlr <njlr@users.noreply.github.com>
2018-10-13 09:38:58 +0100

    * Adds Buckaroo 1.4.0 release

---
+++ buckaroo@1.4.0.rb
@@ -0,0 +1,34 @@
+require "formula"
+
+class BuckarooAT140 < Formula
+
+  @@version = "1.4.0"
+
+  homepage "https://github.com/loopperfect/buckaroo"
+
+  url "https://github.com/LoopPerfect/buckaroo/releases/download/v" + @@version + "/buckaroo-cli.jar"
+  sha256 "9f4c6e2da568ddd16aee3729fbdef1c5f2615722753bc152b43ebd59445b3bbb"
+
+  head "https://github.com/LoopPerfect/buckaroo.git",
+    :using => :git
+
+  depends_on :java => "1.8+"
+  depends_on "buck"
+
+  def install
+    if head? then
+      system "buck", "build", ":buckaroo-cli"
+      libexec.install Dir["buck-out/gen/*"]
+      bin.write_jar_script "#{libexec}/buckaroo-cli.jar", "buckaroo"
+    else
+      libexec.install "buckaroo-cli.jar"
+      bin.write_jar_script libexec/"buckaroo-cli.jar", "buckaroo"
+    end
+  end
+
+  def caveats
+    "Anonymous usage statistics are enabled by default. \n" \
+    "For more information and instructions for disabling analytics, visit: \n" \
+    "https://buckaroo.readthedocs.io/"
+  end
+end
