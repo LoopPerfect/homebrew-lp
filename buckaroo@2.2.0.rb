6186876beb805c9241b252190f40c3706dce29f6
njlr <njlr@users.noreply.github.com>
2019-03-19 22:40:22 +0000

    * Adds Linuxbrew support

--- buckaroo@2.2.0.rb
+++ buckaroo@2.2.0.rb
@@ -6,12 +6,22 @@ class BuckarooAT220 < Formula
 
   homepage "https://buckaroo.pm"
 
-  url "https://github.com/LoopPerfect/buckaroo/releases/download/v" + @@version + "/buckaroo-macos"
-  sha256 "4b81872b58d3aec5a95b0763b51d85a114f5bdff3dfe985cd72bf6e18bfe1ff1"
+  if OS.mac?
+    url "https://github.com/LoopPerfect/buckaroo/releases/download/v" + @@version + "/buckaroo-macos"
+    sha256 "4b81872b58d3aec5a95b0763b51d85a114f5bdff3dfe985cd72bf6e18bfe1ff1"
+  elsif OS.linux?
+    url "https://github.com/LoopPerfect/buckaroo/releases/download/v" + @@version + "/buckaroo-linux"
+    sha256 "23f9a4145a3e51f1205a4b265374c548fc4a4e9156e5121285f5c5bdb4d3228c"
+  end
 
   def install
-    libexec.install "buckaroo-macos"
-    bin.install libexec/"buckaroo-macos" => "buckaroo"
+    if OS.mac?
+      libexec.install "buckaroo-macos"
+      bin.install libexec/"buckaroo-macos" => "buckaroo"
+    elsif OS.linux?
+      libexec.install "buckaroo-linux"
+      bin.install libexec/"buckaroo-linux" => "buckaroo"
+    end
   end
 
   def caveats
